344e4191dea10e3890de9f93f7e858df9cfa12cd
Rodolfo Martínez <rodolfo@agavelab.com>
2017-12-22 17:15:52 -0600

    Created first template

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.views import View
+from django.shortcuts import render
 
 class Home(View):
 
 	def get(self, request, *args, **kwargs):
-		return HttpResponse("<H1>Hola Mundo!</h1>")
+		return render(request, 'home.html')
