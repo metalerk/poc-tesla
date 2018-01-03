7dfc595e9a834e67eb73eeceaf58d273ccf194b0
Luis Esteban <rodriguezjluis0@gmail.com>
2018-01-02 20:48:50 -0800

    fix field name

--- sp/models.py
+++ sp/models.py
@@ -5,8 +5,8 @@ from uuid import uuid4
 class BaseSP(models.Model):
 	id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
 	name = models.CharField(max_length=150)
-	mfal = models.CharField(max_length=150)
-	preference = models.FloatField()
+	mfal = models.CharField(max_length=5)
+	take_rate = models.FloatField()
 
 	def __str__(self):
 		return f'{self.name} - {self.mfal}'
