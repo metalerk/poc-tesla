649467cab6e3bc555e1298c257c5cb61f86817d1
Rodolfo Martínez <rodolfo@agavelab.com>
2018-01-14 01:30:50 -0600

    Added CORS-origin support

--- tesla/settings.py
+++ tesla/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     # 'material',
     # 'material.frontend',
     # 'material.admin',
+    'corsheaders',
     'admin_interface',
     'colorfield',
     'django.contrib.admin',
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -173,3 +175,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
